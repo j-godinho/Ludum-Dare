11e58ba845bab715aaa07a9c456c0f7b0def0a78
j-godinho <joaogodinho_4@hotmail.com>
2015-12-12 05:09:22 +0000

    enemies and borders

--- main.lua
+++ main.lua
@@ -11,6 +11,9 @@ function startGame()
 	player = {x=300, y=400, score=0, speed=200, bulletSpeed=400}
 	enemies = {}
 	enemySpeed=100
+	enemySize=20
+	borders={}
+	ratio=1
 	currentState = "game"
 end
 
@@ -19,7 +22,6 @@ function love.load()
 
   playerImage = love.graphics.newImage("nave.png")
   backgroundImage = love.graphics.newImage("background.jpg")
-  enemyImage = love.graphics.newImage("enemy.png")
 
   coolFont = love.graphics.newFont("ProggySquareTT.ttf", 40)
   love.graphics.setFont(coolFont)
@@ -44,13 +46,13 @@ function love.update(dt)
 
 
 		if math.random() < 0.05 then
-			local newEnemy = {x=800, y=math.random()*800}
+			local newEnemy = {x=800, y=math.random()*800, width=enemySize*ratio, height=enemySize*ratio}
 			table.insert(enemies, newEnemy)
 		end
 
 
 		for i=#enemies,1,-1 do
-			if checkCollision(enemies[i].x, enemies[i].y, enemyImage:getWidth(), enemyImage:getHeight(), player.x, player.y, playerImage:getWidth(), playerImage:getHeight()) then
+			if checkCollision(enemies[i].x, enemies[i].y, enemies[i].width, enemies[i].height, player.x, player.y, playerImage:getWidth(), playerImage:getHeight()) then
 				--gameover
 				currentState="gameover"
 
@@ -60,6 +62,16 @@ function love.update(dt)
 		for i=1, #enemies do
 			enemies[i].x=enemies[i].x-enemySpeed*dt
 		end
+
+
+		local newBorderTop = {x=800, y=0, width=5, height=50*math.random()}
+		table.insert(borders, newBorderTop)
+
+		for i=1, #borders do
+			borders[i].x=borders[i].x-enemySpeed*dt
+		end
+
+
 
 	elseif(currentState=="gameover")then
 		if love.keyboard.isDown("k") then
@@ -76,7 +88,12 @@ function love.draw()
 							love.mouse.getX()-player.x),1,1, playerImage:getWidth()/2, playerImage:getHeight()/2)
 
 		for i=1, #enemies do
-			love.graphics.draw(enemyImage, enemies[i].x, enemies[i].y)
+			love.graphics.rectangle("fill", enemies[i].x-enemies[i].width/2, enemies[i].y-enemies[i].height/2, enemies[i].width, enemies[i].height)
+		end
+
+
+		for i=1, #borders do
+			love.graphics.rectangle("fill", borders[i].x-borders[i].width/2, borders[i].y-borders[i].height/2, borders[i].width, borders[i].height)
 		end
 
 		love.graphics.print(player.score, 20,20)
