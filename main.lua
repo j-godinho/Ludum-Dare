427186a0ba4b2034d9ce95c029e0a132fbcd3119
HugoSilveiro <hugofilipe95@hotmail.com>
2015-12-12 04:02:42 +0000

    Almost

--- main.lua
+++ main.lua
@@ -20,7 +20,6 @@ function love.load()
 
   playerImage = love.graphics.newImage("nave.png")
   backgroundImage = love.graphics.newImage("background.jpg")
-  bulletImage = love.graphics.newImage("bullet.png")
   enemyImage = love.graphics.newImage("enemy.png")
 
   coolFont = love.graphics.newFont("ProggySquareTT.ttf", 40)
